7d74db27ef88cebc1d26725fee9872d7b51286eb
XandaoPe <73856144+XandaoPe@users.noreply.github.com>
2024-02-26 22:40:30 -0300

    correção lista

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getDados() {
+    // return this.http.get('http://localhost:3000/users');
+    return this.http.get('https://clients-back.onrender.com/users');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client-front';
+
+  dados:any=[];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.apiService.getDados().subscribe((res: any) => {
+      console.log('res...', res)
+      this.dados = res;
+    });
+  }
 }
+
